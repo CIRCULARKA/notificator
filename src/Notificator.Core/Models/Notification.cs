fdce00e2ad3b4c032ace0cb79282af8850d88772
4view <akochergin.2002@gmail.com>
2024-06-25 14:43:34 +0500

    Add notification model

---
+++ src/Notificator.Core/Models/Notification.cs
@@ -0,0 +1,39 @@
+﻿namespace Notificator.Core.Models;
+
+public class Notification
+{
+    /// <summary>
+    /// Уникальный идентификатор пользователя
+    /// </summary>
+    public Guid ID { get; set; } 
+
+    /// <summary>
+    /// Уникальный идентификатор пользователя, владеющего уведомлением
+    /// </summary>
+    public Guid OwnerID { get; set; }
+
+    /// <summary>
+    /// Текст уведомления
+    /// </summary>
+    public string Text { get; set; }
+
+    /// <summary>
+    /// Время первой отправки уведомления
+    /// </summary>
+    public DateTimeOffset StartTime { get; set; }
+
+    /// <summary>
+    /// Интервал отправки уведомления
+    /// </summary>
+    public DateTimeOffset Interval { get; set; }
+
+    /// <summary>
+    /// Максимально кол-во отправок уведомления
+    /// </summary>
+    public int MaxAmount { get; set; }
+
+    /// <summary>
+    /// Дни недели, в которые может прийти уведомление
+    /// </summary>
+    public List<int> DaysOfTheWeek { get; set; }
+}
