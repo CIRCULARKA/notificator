14527462203cd428ba51a5b7e917397c6cad2b48
4view <akochergin.2002@gmail.com>
2024-09-02 10:24:15 +0500

    feat: Add method realization

---
+++ src/Notificator.Core/Logging/Implementations/EnvironmentLogsEnricher.cs
@@ -0,0 +1,18 @@
+﻿using Notificator.Core.Logging.Interfaces.ILogsEnricher;
+
+namespace Notificator.Core.Logging.Implementation.EnvironmentLogsEnricher;
+
+public class EnvironmentLogsEnricher : ILogsEnricher
+{
+    string _environment;
+    public EnvironmentLogsEnricher(string environment)
+    {
+        _environment = environment;
+    }
+
+    public string Enrich(string logMessage) 
+    {
+        return $"[{_environment}]" + logMessage;
+    }
+    
+}
