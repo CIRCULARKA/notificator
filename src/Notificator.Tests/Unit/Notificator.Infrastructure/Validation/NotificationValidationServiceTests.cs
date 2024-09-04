2f1949dbd22d409d08a38b9b133f5ed2b803f231
CIRCULARKA <ruslan.getmail@gmail.com>
2024-09-04 08:36:18 +0500

    tests: Add test for notification text validation

--- src/Notificator.Tests/Unit/Notificator.Infrastructure/Validation/NotificationValidationServiceTests.cs
+++ src/Notificator.Tests/Unit/Notificator.Infrastructure/Validation/NotificationValidationServiceTests.cs
@@ -1,6 +1,31 @@
-using Xunit;
+namespace Notificator.Tests.Unit.Infrastructure.Validation;
 
 public class NotificationValidationServiceTests
 {
-    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Validate_WhenNotificationTextIsEmpty_Throws(string notificationText)
+    {
+        // Arrange
+        var settings = new NotificationValidationSettings();
+        var service = new NotificationValidationService(settings);
+
+        var notification = new NotificationDto
+        {
+            StartTime = new DateTime(2099, 01, 01),
+            Header = "nevermind",
+            Text = notificationText
+        };
+
+        Exception actualException = null;
+
+        // Act
+        try { service.Validation(notification); }
+        catch (Exception e) { actualException = e; }
+
+        // Assert
+        Assert.NotNull(actualException);
+    }
 }
--- src/Notificator.Tests/Usings.cs
+++ src/Notificator.Tests/Usings.cs
@@ -1,3 +1,5 @@
+global using Moq;
+global using Xunit;
 global using Notificator.Core.Models;
 global using Notificator.Core.Validation;
-global using Xunit;
+global using Notificator.Infrastructure.Validation;
