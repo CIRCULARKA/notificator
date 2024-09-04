96c4eb0782b7b9ab62d6944db1b0bcaa2759f6f3
CIRCULARKA <ruslan.getmail@gmail.com>
2024-09-04 08:37:02 +0500

    docs: Add summary

--- src/Notificator.Core/Validation/InotificationValidationService.cs
+++ src/Notificator.Core/Validation/InotificationValidationService.cs
@@ -1,9 +1,12 @@
 namespace Notificator.Core.Validation;
 
 /// <summary>
-/// 
+/// Interface for validation service
 /// </summary>
 public interface INotificationValidationService
 {
+    /// <summary>
+    /// Validates notification
+    /// </summary>
     public void Validation(NotificationDto notificationDto);
 }
