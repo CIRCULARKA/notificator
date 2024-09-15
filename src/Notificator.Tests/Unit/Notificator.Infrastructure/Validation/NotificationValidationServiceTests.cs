1d6544d7474acf1fbf4a11ee7de5d9d76068fca2
4view <akochergin.2002@gmail.com>
2024-09-15 21:04:25 +0500

    feat: add tests

--- src/Notificator.Tests/Unit/Notificator.Infrastructure/Validation/NotificationValidationServiceTests.cs
+++ src/Notificator.Tests/Unit/Notificator.Infrastructure/Validation/NotificationValidationServiceTests.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualBasic;
+using Xunit.Sdk;
+
 namespace Notificator.Tests.Unit.Infrastructure.Validation;
 
 public class NotificationValidationServiceTests
@@ -28,4 +31,57 @@ public class NotificationValidationServiceTests
         // Assert
         Assert.NotNull(actualException);
     }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("  ")]
+    public void Validate_WhenNotificationHeaderIsRequired_Throws(string notificationHeader)
+    {
+        // Arrange
+        var settings = new NotificationValidationSettings();
+        settings.IsHeaderRequired = true;
+        var service = new NotificationValidationService(settings);
+
+
+        var notification = new NotificationDto
+        {
+            Text = "hghgh",
+            Header = notificationHeader
+        };
+
+        Exception actualeExeption = null;
+
+        // Act
+        try { service.Validation(notification); }
+        catch (Exception e) { actualeExeption = e; }
+
+        // Assert
+        Assert.NotNull(actualeExeption);
+    }  
+
+    [Fact]
+    public void Validate_WhenNotificationStartTimeIsNotEmptyAndLaterThenDateTimeNow_Throws()
+    {
+        // Arrange
+        var settings = new NotificationValidationSettings();
+        
+        var service = new NotificationValidationService(settings);
+
+        var notification = new NotificationDto
+        {
+            Text = "whatever",
+            Header = "whatever",
+            StartTime = new DateTimeOffset(2099, 09, 04, 0, 0, 0, TimeSpan.FromDays(0))
+        };
+
+        Exception actualExeption = null;
+
+        // Act
+        try { service.Validation(notification); }
+        catch (Exception e) { actualExeption = e; }
+
+        // Assert
+        Assert.NotNull(actualExeption);
+    } 
 }
