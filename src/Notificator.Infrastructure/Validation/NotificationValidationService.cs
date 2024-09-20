3da56ba791ddfd37c897ffe7530026c3540199fb
4view <akochergin.2002@gmail.com>
2024-09-20 10:30:35 +0500

    feat: Add validation

--- src/Notificator.Infrastructure/Validation/NotificationValidationService.cs
+++ src/Notificator.Infrastructure/Validation/NotificationValidationService.cs
@@ -26,5 +26,11 @@ public class NotificationValidationService : INotificationValidationService
             throw new InvalidOperationException("Notification header must not be empty");
         if (notificationDto.StartTime < _timeService.CurrentTime)
             throw new InvalidOperationException("Notification start time must be greater then current time");
+        if (notificationDto.MaxAmount < 0)
+            throw new InvalidOperationException("Notificator max amount can`t be less then 0");
+        if (notificationDto.Interval != null || notificationDto.Interval < _settings.MinInterval)
+            throw new InvalidOperationException("Notificator interval must be empty or interval value must be greater then MinInterval");
+        //if (notificationDto.DaysOfTheWeek != null || )
+
     }
 }
