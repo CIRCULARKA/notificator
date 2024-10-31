ca450ddac4cb5648df3c32241eba100abcde7bc5
CIRCULARKA <ruslan.getmail@gmail.com>
2024-10-31 16:35:43 +0500

    feat: Add implementation template

---
+++ src/Notificator.Infrastructure/Inspection/NotificationInspector.cs
@@ -0,0 +1,12 @@
+namespace Notificator.Infrastructure.Inspection;
+
+/// <summary>
+/// Объект, ответственный за определение статуса уведомлений
+/// </summary>
+public class NotificationInspector : INotificationInspector
+{
+    public bool ShouldBeCompleted(Notification notification)
+    {
+        throw new NotImplementedException();
+    }
+}
