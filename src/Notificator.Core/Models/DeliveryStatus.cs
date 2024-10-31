d2019dbb71ae816a150bf8b8ffbb0ea8087d9577
CIRCULARKA <ruslan.getmail@gmail.com>
2024-10-31 16:35:27 +0500

    feat: Describe sent notification

---
+++ src/Notificator.Core/Models/DeliveryStatus.cs
@@ -0,0 +1,17 @@
+namespace Notificator.Core.Models;
+
+/// <summary>
+/// Статусы отправки
+/// </summary>
+public enum DeliveryStatus
+{
+    /// <summary>
+    /// Отправка успешна
+    /// </summary>
+    Successfull,
+
+    /// <summary>
+    /// Отправка завершилась ошибкой
+    /// </summary>
+    Error
+}
---
+++ src/Notificator.Core/Models/SentNotification.cs
@@ -0,0 +1,28 @@
+namespace Notificator.Core.Models;
+
+/// <summary>
+/// Описывает уведомление, которое было отправлено
+/// </summary>
+public class SentNotification
+{
+    public void Incel() { return; }
+    /// <summary>
+    /// Когда уведомление было отправлено
+    /// </summary>
+    public DateTimeOffset TimeSent { get; }
+
+    /// <summary>
+    /// Уведомление, которое было отправлено
+    /// </summary>
+    public Notification Notification { get; }
+
+    /// <summary>
+    /// Статус отправки уведомления
+    /// </summary>
+    public DeliveryStatus Status { get; }
+
+    /// <summary>
+    /// Подробности ошибки, которая произошла во время отправки уведомления
+    /// </summary>
+    public string ErrorMessage { get; }
+}
