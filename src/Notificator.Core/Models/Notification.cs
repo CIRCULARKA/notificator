d107674a008713dfca90c22bf52bebf723168af9
CIRCULARKA <ruslan.getmail@gmail.com>
2024-10-31 23:24:57 +0500

    feat: Remove excess property and make props initiable

--- src/Notificator.Core/Models/Notification.cs
+++ src/Notificator.Core/Models/Notification.cs
@@ -8,45 +8,40 @@ public class Notification
     /// <summary>
     /// Заголовок уведомления 
     /// </summary>
-    public string Header { get; }
+    public string Header { get; init; }
 
     /// <summary>
     /// Текст уведомления
     /// </summary>
-    public string Text { get; }
+    public string Text { get; init; }
 
     /// <summary>
     /// Время первой отправки уведомления
     /// </summary>
-    public DateTimeOffset StartTime { get; }
+    public DateTimeOffset StartTime { get; init; }
 
     /// <summary>
     /// Время последней отправки уведомления
     /// </summary>
-    public DateTime? EndTime { get; }
+    public DateTime? EndTime { get; init; }
 
     /// <summary>
     /// Интервал отправки уведомления
     /// </summary>
-    public TimeSpan? Interval { get; }
+    public TimeSpan? Interval { get; init; }
 
     /// <summary>
     /// Максимально кол-во отправок уведомления
     /// </summary>
-    public int MaxAmount { get; }
+    public int MaxAmount { get; init; }
 
     /// <summary>
     /// Дни недели, в которые может прийти уведомление
     /// </summary>
-    public List<int> DaysOfTheWeek { get; }
-
-    /// <summary>
-    /// Выполненные уведомления
-    /// </summary>
-    public bool IsCompleted { get; }
+    public List<int> DaysOfTheWeek { get; init; }
 
     /// <summary>
     /// История отправки этого уведомления
     /// </summary>
-    public List<SentNotification> History { get; }
+    public List<SentNotification> History { get; init; }
 }
