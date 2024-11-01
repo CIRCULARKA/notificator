8eaa277f6878a3361a39e77e39a97664ce4a798d
4view <akochergin.2002@gmail.com>
2024-11-02 00:29:30 +0500

    style: add spaces

--- src/Notificator.Infrastructure/Validation/NotificationValidationService.cs
+++ src/Notificator.Infrastructure/Validation/NotificationValidationService.cs
@@ -22,20 +22,28 @@ public class NotificationValidationService : INotificationValidationService
     {
         if (string.IsNullOrWhiteSpace(notificationDto.Text))
             throw new InvalidOperationException("Notification's text must not be empty");
+
         if (string.IsNullOrWhiteSpace(notificationDto.Header) && _settings.IsHeaderRequired)
             throw new InvalidOperationException("Notification's header must not be empty");
+
         if (notificationDto.StartTime > _timeService.CurrentTime)
             throw new InvalidOperationException("Notification's start time must be greater then current time");
+
         if (notificationDto.MaxAmount <= 0)
             throw new InvalidOperationException("Max amount of notifications must be greater than zero");
+
         if (notificationDto.Interval != null || notificationDto.Interval < _settings.MinInterval)
             throw new InvalidOperationException("Notification's interval must not be less than the minimal interval of {_settings.MinInterval}");
+
         if (notificationDto.EndTime != null && notificationDto.EndTime < notificationDto.StartTime)
             throw new InvalidOperationException("Notification's end time must be greater then start time");
+
         if ((notificationDto.StartTime - notificationDto.EndTime) < _settings.MinInterval)
             throw new InvalidOperationException("Notification's {_settings.MinInterval} must be greater than difference between start time and end time");
+
         if (notificationDto.DaysOfTheWeek == null)
             return;
+            
         notificationDto.DaysOfTheWeek.ForEach(day => 
         {
             if (day < 1 || day > 7)
